baa6a8e286bf7f1a2575d496cc2f685fb0e69a2f
Sahitya Modi <sahityamodi0@gmail.com>
2023-11-01 02:10:03 +0530

    create sign up route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "../api/routes/user.routes.js";
+import authRouter from "../api/routes/auth.routes.js";
+
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -16,6 +19,7 @@ mongoose
   });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
