c7b36130722ed6006efb38aed4bacfc5c057dff1
Sahitya Modi <sahityamodi0@gmail.com>
2023-11-10 01:06:12 +0530

    image upload functionality

--- client/src/components/PrivateRoutes.jsx
+++ client/src/components/PrivateRoutes.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-export const PrivateRoutes = () => {
+const PrivateRoutes = () => {
   const { currentUser } = useSelector((state) => state.user);
   return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 };
+
+export default PrivateRoutes;
